a41f50b7863e93ffe121a57520c720b03774dc02
Dmitry <takesxq77@gmail.com>
2024-11-29 22:30:54 +0300

    +template

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -10,12 +10,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
    
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        std::vector<Document> result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        AddToDeq(result.empty());
-        return result;
-    }
-
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) ;
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
 
@@ -32,3 +27,9 @@ private:
     static constexpr int min_in_day_ = 1440;
     const SearchServer& search_server_;
 };
+ template <typename DocumentPredicate>
+    std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate){
+        std::vector<Document> result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        AddToDeq(result.empty());
+        return result;
+    }
