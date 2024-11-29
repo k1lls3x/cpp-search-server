be940b43c6c74045c75d85378ea2610456d77e48
k1lls3x <118112928+k1lls3x@users.noreply.github.com>
2024-11-29 20:14:12 +0300

    Fourth sprint 2.0

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -8,8 +8,7 @@
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
-    
-    // Шаблонный метод определён полностью в заголовочном файле
+   
     template <typename DocumentPredicate>
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
         std::vector<Document> result = search_server_.FindTopDocuments(raw_query, document_predicate);
